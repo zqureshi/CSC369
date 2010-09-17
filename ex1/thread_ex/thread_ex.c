2eedd56bfe0032b1f19300585f3352a9c50bc208
Zeeshan Qureshi <zee@zqureshi.in>
2010-09-17 05:00:05 +0000

    started Pthreads question
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc369-2010-09/g0zee@5 ed4f7a11-d548-43f3-88d0-706ec5dcd84b

---
+++ ex1/thread_ex/thread_ex.c
@@ -0,0 +1,17 @@
+/* 
+ * CSC 369 Fall 2010 - Excercise 1
+ *
+ * Zeeshan Qureshi
+ * zeeshan.qureshi@utoronto.ca
+ * $Id$
+ */
+
+#include<pthread.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv){
+  printf("Hello World!!\n");
+
+  return 0;
+}
