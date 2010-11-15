a2c39c114861e71ceff19a0a1f7327c383f71fc2
Zeeshan Qureshi <zee@zqureshi.in>
2010-11-15 22:45:21 +0000

    run tests for different values of DATASIZE
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc369-2010-09/g0zee@30 ed4f7a11-d548-43f3-88d0-706ec5dcd84b

--- ex3/pageTest.sh
+++ ex3/pageTest.sh
@@ -2,21 +2,30 @@
 
 echo -e "VM Page Fault Stress Test\n"
 
-for num_ops in 10000 50000 100000 500000
+for data_size in 8 16 32 64 128 256
 do
-  i=0
-  while [ ${i} -lt 10 ]
+  echo -e "DATASIZE: ${data_size}:\n"
+  export DATASIZE=${data_size}
+  make -s clean
+  make -s -e
+
+  for num_ops in 10000 50000 100000 500000
   do
-    echo -e "${num_ops} ops, run ${i}:\n"
+    i=0
+    while [ ${i} -lt 3 ]
+    do
+      echo -e "${num_ops} ops, run ${i}:"
 
-    echo 'testheap:'
-    ./testheap ${num_ops}
-    echo ''
+      echo 'testheap:'
+      ./testheap ${num_ops}
+      echo ''
 
-    echo 'testbh:'
-    ./testbh ${num_ops}
-    echo ''
+      echo 'testbh:'
+      ./testbh ${num_ops}
+      echo '----------------------------------------------------------------'
+      echo ''
 
-    i=$(( ${i} + 1 ))
+      i=$(( ${i} + 1 ))
+    done
   done
 done
