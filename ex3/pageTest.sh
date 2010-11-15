a2f33b10f8f0819b856b4a16e0b8faf876d556c7
Zeeshan Qureshi <zee@zqureshi.in>
2010-11-15 22:45:26 +0000

    modified output of script
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc369-2010-09/g0zee@31 ed4f7a11-d548-43f3-88d0-706ec5dcd84b

--- ex3/pageTest.sh
+++ ex3/pageTest.sh
@@ -4,17 +4,18 @@ echo -e "VM Page Fault Stress Test\n"
 
 for data_size in 8 16 32 64 128 256
 do
-  echo -e "DATASIZE: ${data_size}:\n"
+  echo "DATASIZE: ${data_size}"
   export DATASIZE=${data_size}
   make -s clean
-  make -s -e
+  make -e
+  echo ''
 
   for num_ops in 10000 50000 100000 500000
   do
     i=0
     while [ ${i} -lt 3 ]
     do
-      echo -e "${num_ops} ops, run ${i}:"
+      echo -e "DATASIZE: ${data_size}, ${num_ops} ops, run $(( ${i} + 1 )):"
 
       echo 'testheap:'
       ./testheap ${num_ops}
